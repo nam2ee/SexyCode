ea81fc3f951d2dde1b66bc95008267c08812ca63
nam2ee <nyj5404@yonsei.ac.kr>
2024-06-03 18:24:55 +0900

    IO

--- lifetime2/src/main.rs
+++ lifetime2/src/main.rs
@@ -1,7 +1,9 @@
-struct Book {
-    title: &str,
-    author: &str,
+#[derive(Debug)]
+struct Book<'a> {
+    title: &'a str,
+    author: &'a str,
 }
+
 
 fn main() {
     let title = String::from("The Rust Programming Language");
@@ -16,12 +18,31 @@ fn main() {
         };
     } // short_title의 라이프타임이 여기서 끝남
 
-    println!("{} by {}", book.title, book.author); // 유효하지 않은 참조를 사용하려 함
+    println!("{} by {}", book.title, book.author); // 유효하지 않은 참조 아님! 
+                                                    // 데이터 자체는 title에 있으므로 유효함
 
 
     // 그래서 재귀적으로 되어있어서
     // 명시적으로 라이프타임을 달지 않으면 그런 검사를 못하는데,
     // 명시적으로 달아준다면 로직이 생겨서 검사할 수 있다 ! 
+
+    /* 이제 문제가 되는 예시를 알아보자!  */
+
+    let book;
+
+    {
+
+        let title = String::from("Hello");
+        let title2 = String::from("World");
+
+        
+        book = Book {
+            title: &title,
+            author: &title2,
+        };
+        
+    }
+    //println!("{:?}", book);
 }
 
 
