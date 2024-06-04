f2ab3943bc72c9cc442483a2380522e85305d567
nam2ee <nyj5404@yonsei.ac.kr>
2024-06-04 11:00:55 +0900

    ModulIo

--- ModulIO/src/lib.rs
+++ ModulIO/src/lib.rs
@@ -1,5 +1,67 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::fs::File;
+use std::io::prelude::*;
+use std::error::Error;
+
+
+pub fn run(config: &Config) -> Result<(), Box<dyn Error> > {
+
+    let mut f = File::open(&config.filename).expect("file not found");
+
+    let mut contents = String::new();
+
+    f.read_to_string(&mut contents)?;
+
+    println!("With text:\n{}", contents);
+
+    Ok(())
+
+}
+
+/*
+
+struct Config<'a> {
+    query: &'a String,
+    filename: &'a String
+}
+- 이렇게 구현할 수도 있지만, 조금 더 깔끔하게 하기 위해서
+참조가 아니라 아예 데이터를 복사하는 (깊은복사) clone을 쓴다 
+*/
+#[derive(Debug)]
+pub struct Config{
+    pub query: String,
+    pub filename: String
+}
+
+impl Config{
+
+    #[warn(dead_code)]
+    pub fn parse_config(list: &[String]) -> Config{
+    let query = list[1].clone();
+    // &list[1].clone()을 하면 list[1]이 아니라
+    // 참조를 복사하므로 안된다! 
+    let filename = list[2].clone();
+
+    let result = Config{
+        query,filename,
+    };
+    result
+    }
+
+    pub fn new(list: &[String]) -> Result<Config,&str> {
+    
+    if list.len()<3
+    {
+        return Err("not enough args!");
+        // 문자열 리터럴은 'static이다! 
+    }
+
+    let query = list[1].clone();
+    // &list[1].clone()을 하면 list[1]이 아니라
+    // 참조를 복사하므로 안된다! 
+    let filename = list[2].clone();
+
+    Ok(Config{query,filename})
+    }
 }
 
 #[cfg(test)]
@@ -8,7 +70,6 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        
     }
 }
---
+++ ModulIO/target/debug/deps/ModulIO-3ea90f242c303a50.d
@@ -0,0 +1,5 @@
+/Users/yoonjae/SexyCode/ModulIO/target/debug/deps/libModulIO-3ea90f242c303a50.rmeta: src/lib.rs
+
+/Users/yoonjae/SexyCode/ModulIO/target/debug/deps/ModulIO-3ea90f242c303a50.d: src/lib.rs
+
+src/lib.rs:
